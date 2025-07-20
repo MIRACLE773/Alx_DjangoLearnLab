82ad9843524e789b919041969277eaa225e884a1
MIRACLE773 <mjschool48@gmail.com>
2025-07-19 20:55:59 -0700

    feat: added models and migrated

--- LibraryProject/relationship_app/admin.py
+++ LibraryProject/relationship_app/admin.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from .models import Author, Book, Library, Librarian
 
--- LibraryProject/relationship_app/models.py
+++ LibraryProject/relationship_app/models.py
@@ -2,34 +2,33 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
-    
-    
+    biography = models.TextField(blank=True)
+
     def __str__(self):
         return self.name
-    
-    
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    
-    
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    library = models.ForeignKey(Library, on_delete=models.CASCADE, related_name='books')
+    published_date = models.DateField()
+
     def __str__(self):
         return self.title
-    
-    
-class Library(models.Model):
-    name = models.CharField(max_length=50)
-    books = models.ManyToManyField(Book)
-    
-    
-    def __str__(self):
-        return self.name
-    
-    
+
+
 class Librarian(models.Model):
-    name = models.CharField(max_length=90)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
-    
-    
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+
     def __str__(self):
         return self.name
