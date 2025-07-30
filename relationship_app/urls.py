1ee2339d2bebfeeb2d2e433d91613e66cc5283c0
MIRACLE773 <mjschool48@gmail.com>
2025-07-30 15:22:44 -0700

    Initial commit: ALX Role-Based Access Control project

---
+++ relationship_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin-only/', views.admin_view, name='admin_view'),
+    path('librarian-only/', views.librarian_view, name='librarian_view'),
+    path('member-only/', views.member_view, name='member_view'),
+]
