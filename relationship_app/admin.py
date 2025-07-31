7f6fc1551391dc97f4f9c2ef887e15ed8124dd8b
MIRACLE773 <mjschool48@gmail.com>
2025-07-30 22:56:54 -0700

    Fix: cleaned merge conflict in admin.py

--- relationship_app/admin.py
+++ relationship_app/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
-<<<<<<< HEAD
-
-# Register your models here.
-=======
 from .models import UserProfile, Library, Book
 
 admin.site.register(UserProfile)
 admin.site.register(Library)
 admin.site.register(Book)
->>>>>>> 3d6844a3686b4fb9f53fc556c1492935bba6b3f2
