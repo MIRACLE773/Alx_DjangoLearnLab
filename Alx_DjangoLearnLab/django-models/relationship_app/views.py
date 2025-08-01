03f496e4737e98ad9dfaed5d2b03ae4aaf9150f6
MIRACLE773 <mjschool48@gmail.com>
2025-08-01 14:30:10 -0700

    Fix: removed nonexistent published_date from BookForm

--- Alx_DjangoLearnLab/django-models/relationship_app/views.py
+++ Alx_DjangoLearnLab/django-models/relationship_app/views.py
@@ -8,7 +8,7 @@ def book_list(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/book_list.html', {'books': books})
 
-@permission_required('relationship_app.can_add_book', raise_exception=True)
+@permission_required('relationship_app.canaddbook', raise_exception=True)
 def add_book(request):
     if request.method == 'POST':
         form = BookForm(request.POST)
@@ -19,7 +19,7 @@ def add_book(request):
         form = BookForm()
     return render(request, 'relationship_app/add_book.html', {'form': form})
 
-@permission_required('relationship_app.can_change_book', raise_exception=True)
+@permission_required('relationship_app.canchangebook', raise_exception=True)
 def edit_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
@@ -31,10 +31,11 @@ def edit_book(request, pk):
         form = BookForm(instance=book)
     return render(request, 'relationship_app/edit_book.html', {'form': form})
 
-@permission_required('relationship_app.can_delete_book', raise_exception=True)
+@permission_required('relationship_app.candeletebook', raise_exception=True)
 def delete_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
         book.delete()
         return redirect('book_list')
     return render(request, 'relationship_app/delete_book.html', {'book': book})
+
