9939088ee93d4f1d7d502287238f8fd2b55b445d
MIRACLE773 <mjschool48@gmail.com>
2025-08-05 02:26:16 -0700

    Implement custom user model with admin, view, and template

--- advanced_features_and_security/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "users",
+    "bookshelf",
 ]
 
 MIDDLEWARE = [
@@ -123,4 +123,3 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'bookshelf.CustomUser'
-
